2d4c8e46e1499e0b4f1e763395b3c1da4f391a23
Cedric Ziel <cedric@cedric-ziel.com>
2017-01-05 20:43:45 +0100

    Use correct signal name

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,9 +4,8 @@ defined('TYPO3_MODE') or die();
 /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 $signalSlotDispatcher->connect(
-    'In2code\Femanager\Controller\NewController',
+    'In2code\Femanager\Controller\AbstractController',
     'finalCreateAfterPersist',
     CedricZiel\FemanagerForceForum\Slot\FemanagerForceRecordTypeSlot::class,
-    'force',
-    FALSE
+    'force'
 );
